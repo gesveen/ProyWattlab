115663c19b27bebaca1bc894c5b25d4ecd1a8341
callmemapa <maria.paula.mosquera@correounivalle.edu.co>
2020-03-06 16:15:30 -0500

    pruebita

--- srcgui/src/componentes/InformacionServicios.js
+++ srcgui/src/componentes/InformacionServicios.js
@@ -53,12 +53,12 @@ class InformacionServicios extends React.Component {
                             <div className="col-md-4">
                                 <div className="card mb-4 shadow-sm">
                                     <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="https://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Servicio 3">
-                                        <title>Servicio 3</title>
+                                        <title>Servicio 3 ma g</title>
                                         <rect width="100%" height="100%" fill="#55595c"/>
                                         <text x="50%" y="50%" fill="#eceeef" dy=".3em">Servicio 3</text>
                                     </svg>
                                     <div className="card-body">
-                                        <p className="card-text">Servicio 3</p>
+                                        <p className="card-text">Servicio 3 ma g</p>
                                         <div className="d-flex justify-content-between align-items-center">
                                             <div className="btn-group">
                                                 <button type="button" className="btn btn-sm btn-outline-success">Leer</button>

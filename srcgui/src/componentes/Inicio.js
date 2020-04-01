c85acfe9e0f269698dced15d649f11172342ea0f
mariaPaula <maria.paula.mosquera@correounivalle.edu.co>
2020-04-01 01:01:49 -0500

    cambios en el el panel de ingreso pdta: intenté más pero el pc se pifeó

---
+++ srcgui/src/componentes/Inicio.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import { useTranslation } from 'react-i18next';
+import CardOpt from './CardOpt';
+
+function Inicio() {
+
+ const i18n = useTranslation();
+    return(
+        <div className="container">
+            <div>
+              <h1>Inicio</h1>
+            </div>
+            <div className="row" style={{textAlign: "center"}}>
+                <CardOpt
+                modulo = "Usuarios"
+                urlRuta = "/ModuloAdministrador/Users"
+                color = "#5fb677"
+                d1 = "M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 100-6 3 3 0 000 6zm-5.784 6A2.238 2.238 0 015 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 005 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 100-5 2.5 2.5 0 000 5z"
+                icono = "bi bi-people-fill"/>
+
+                <CardOpt
+                modulo = "Transformadores"
+                urlRuta = "/ModuloAdministrador/Transformadores"
+                color = "#91c470"
+                d1 = "M8 16s6-5.686 6-10A6 6 0 002 6c0 4.314 6 10 6 10zm0-7a3 3 0 100-6 3 3 0 000 6z"
+                icono ="bi bi-geo-alt"/>
+
+                <CardOpt
+                modulo = "Reportes"
+                urlRuta = "/ModuloAdministrador/Reportes"
+                color = "#bed57e"
+                d = "M0 0h1v16H0V0zm1 15h15v1H1v-1z"
+                d1 = "M14.39 4.312L10.041 9.75 7 6.707l-3.646 3.647-.708-.708L7 5.293 9.959 8.25l3.65-4.563.781.624z"
+                d2 = "M10 3.5a.5.5 0 01.5-.5h4a.5.5 0 01.5.5v4a.5.5 0 01-1 0V4h-3.5a.5.5 0 01-.5-.5z"
+                icono = "bi bi-graph-up"/>
+
+                <CardOpt
+                modulo = "Pagos"
+                urlRuta = "/ModuloAdministrador/Pagos"
+                color = "#dddd95"
+                d1 = "M.125 11.17A.5.5 0 01.5 11H6a.5.5 0 01.5.5 1.5 1.5 0 003 0 .5.5 0 01.5-.5h5.5a.5.5 0 01.496.562l-.39 3.124A1.5 1.5 0 0114.117 16H1.883a1.5 1.5 0 01-1.489-1.314l-.39-3.124a.5.5 0 01.121-.393zM3.81.563A1.5 1.5 0 014.98 0h6.04a1.5 1.5 0 011.17.563l3.7 4.625a.5.5 0 01-.78.624l-3.7-4.624A.5.5 0 0011.02 1H4.98a.5.5 0 00-.39.188L.89 5.812a.5.5 0 11-.78-.624L3.81.563z"
+                d2 = "M.125 5.17A.5.5 0 01.5 5H6a.5.5 0 01.5.5 1.5 1.5 0 003 0A.5.5 0 0110 5h5.5a.5.5 0 01.496.562l-.39 3.124A1.5 1.5 0 0114.117 10H1.883A1.5 1.5 0 01.394 8.686l-.39-3.124a.5.5 0 01.121-.393z"
+                icono = "bi bi-inboxes-fill"/>
+
+                <CardOpt
+                modulo = "Configuración"
+                urlRuta = "/ModuloAdministrador/Configuracion"
+                color = "#5fb677"
+                d1 = "M0 1l1-1 3.081 2.2a1 1 0 01.419.815v.07a1 1 0 00.293.708L10.5 9.5l.914-.305a1 1 0 011.023.242l3.356 3.356a1 1 0 010 1.414l-1.586 1.586a1 1 0 01-1.414 0l-3.356-3.356a1 1 0 01-.242-1.023L9.5 10.5 3.793 4.793a1 1 0 00-.707-.293h-.071a1 1 0 01-.814-.419L0 1zm11.354 9.646a.5.5 0 00-.708.708l3 3a.5.5 0 00.708-.708l-3-3z"
+                d2 = "M15.898 2.223a3.003 3.003 0 01-3.679 3.674L5.878 12.15a3 3 0 11-2.027-2.027l6.252-6.341A3 3 0 0113.778.1l-2.142 2.142L12 4l1.757.364 2.141-2.141zm-13.37 9.019L3.001 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026z"
+                icono = "bi bi-tools"/>
+
+          </div>
+       </div>
+    )
+}
+
+export default Inicio;
